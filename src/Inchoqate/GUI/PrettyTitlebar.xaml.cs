687f5e55b0272ad63bfef967fc95e07ce79c5c9f
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-04 20:04:08 +0200

    xaml windowchrome

---
+++ src/Inchoqate/GUI/PrettyTitlebar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inchoqate.GUI
+{
+    /// <summary>
+    /// Interaction logic for PrettyTitlebar.xaml
+    /// </summary>
+    public partial class PrettyTitlebar : UserControl
+    {
+        public Window ContentWindow { get; private set; }
+
+
+        public void Initiate(Window contentWindow)
+        {
+            ContentWindow = contentWindow;
+
+            Background = Brushes.Red;
+        }
+
+#pragma warning disable CS8618 
+        public PrettyTitlebar()
+        {
+            InitializeComponent();
+        }
+#pragma warning restore CS8618 
+    }
+}
