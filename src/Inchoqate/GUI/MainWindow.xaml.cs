d6e65b4083906dc3352f5e95f0ab1baab081c913
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-04 17:14:03 +0200

    Update MainWindow.xaml.cs

--- src/Inchoqate/GUI/MainWindow.xaml.cs
+++ src/Inchoqate/GUI/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Extensions.Logging;
 using OpenTK.Graphics.OpenGL4;
 using System.IO;
 using OpenTK.Compute.OpenCL;
