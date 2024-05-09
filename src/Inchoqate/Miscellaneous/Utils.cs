030a419ea235ea43045c6de34a0166e213292db1
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-09 20:22:43 +0200

    Create Utils.cs

---
+++ src/Inchoqate/Miscellaneous/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscellaneous
+{
+    public class Utils
+    {
+        public static double Lerp(double min, double max, double t)
+        {
+            return min + (max - min) * t;
+        }
+    }
+}
