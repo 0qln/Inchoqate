cf717a5074e76f132417fb1fd5141417a882de86
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-10 23:39:01 +0200

    Update ActionButtonOption

--- src/Inchoqate/GUI/Titlebar/ActionButtonOption.xaml.cs
+++ src/Inchoqate/GUI/Titlebar/ActionButtonOption.xaml.cs
@@ -1,28 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace GUI.Titlebar
+
+namespace Inchoqate.GUI.Titlebar
 {
     /// <summary>
     /// Interaction logic for ActionButtonOption.xaml
     /// </summary>
     public partial class ActionButtonOption : UserControl
     {
+        public static readonly DependencyProperty ShortcutProperty = DependencyProperty.Register(
+            "Shortcut", typeof(KeyboardShortcut), typeof(ActionButtonOption));
+
+        public KeyboardShortcut Shortcut
+        {
+            get => (KeyboardShortcut)GetValue(ShortcutProperty);
+            set => SetValue(ShortcutProperty, value);
+        }
 
 
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof(Image), typeof(ActionButtonOption));
 
+        public Image Icon
+        {
+            get => (Image)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            "Title", typeof(string), typeof(ActionButtonOption));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command", typeof(ICommand), typeof(ActionButtonOption));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
 
 
         public ActionButtonOption()
