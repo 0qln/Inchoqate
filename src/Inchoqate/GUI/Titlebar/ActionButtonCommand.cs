a8ba230eeb2f9d4e6d6640e3aeac8505fe5e8aea
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-10 23:38:33 +0200

    Create ActionButtonCommand.cs

---
+++ src/Inchoqate/GUI/Titlebar/ActionButtonCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Inchoqate.GUI.Titlebar
+{
+    public class ActionButtonCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action _action;
+
+        public ActionButtonCommand(Action action)
+        {
+            _action = action;
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            _action();
+        }
+    }
+}
