154a2d2aa72215c7fa2bffa9876138ff7552978e
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-17 00:58:16 +0200

    Update Texture.cs

--- src/Inchoqate/GUI/Texture.cs
+++ src/Inchoqate/GUI/Texture.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.Extensions.Logging;
 using Miscellaneous.Logging;
 using OpenTK.Graphics.OpenGL4;
@@ -11,13 +6,12 @@ using StbImageSharp;
 
 namespace Inchoqate.GUI
 {
-    public class Texture
+    public class Texture : IDisposable
     {
-        private readonly ILogger<Texture> _logger = FileLoggerFactory.CreateLogger<Texture>();
+        private static readonly ILogger<Texture> _logger = FileLoggerFactory.CreateLogger<Texture>();
 
-        public readonly int Handle;
-        public readonly int Width;
-        public readonly int Height;
+        public readonly int Handle;        
+        public readonly int Width, Height;
 
 
         public Texture(string path)
@@ -34,7 +28,7 @@ namespace Inchoqate.GUI
             Width = image.Width;
             Height = image.Height;
             GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, Width, Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
-
+            
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 
@@ -43,8 +37,7 @@ namespace Inchoqate.GUI
 
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
-
-            _logger.LogInformation($"Created texture from file: {path}");
+            _logger.LogInformation("Created texture from file: {path}", path);
         }
 
 
@@ -63,7 +56,7 @@ namespace Inchoqate.GUI
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
 
-            _logger.LogInformation($"Created empty texture with dimensions: {width}x{height}");
+            _logger.LogInformation("Created empty texture with dimensions: {width}x{height}", width, height);
         }
 
 
@@ -72,5 +65,40 @@ namespace Inchoqate.GUI
             GL.ActiveTexture(unit);
             GL.BindTexture(TextureTarget.Texture2D, Handle);
         }
+
+
+        #region Clean up
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteTexture(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Texture()
+        {
+            // https://www.khronos.org/opengl/wiki/Common_Mistakes#The_Object_Oriented_Language_Problem
+            // The OpenGL resources have to be released from a thread with an active OpenGL Context.
+            // The GC runs on a seperate thread, thus releasing unmanaged GL resources inside the finalizer
+            // is not possible.
+            if (disposedValue == false)
+            {
+                _logger.LogWarning("GPU Resource leak! Did you forget to call Dispose()?");
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
     }
 }
