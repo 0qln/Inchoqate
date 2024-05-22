e039d5bb80e484ab4d926324e4d47c80f15c981b
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-22 18:34:24 +0200

    Add logging system

---
+++ src/Inchoqate/GUI/Logging/FileLoggerFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Inchoqate.Logging
+{
+    public static class FileLoggerFactory
+    {
+        private static readonly ILoggerFactory _factory;
+        private static readonly StreamWriter _writer;
+
+        static FileLoggerFactory()
+        {
+            string logFilePath = "log.txt";
+            _writer = new(logFilePath, append: true);
+            _factory = LoggerFactory.Create(builder =>
+            {
+                builder.AddProvider(new FileLoggerProvider(_writer));
+            });
+
+            _factory
+                .CreateLogger("Logging")
+                .LogInformation("File logger initiated.");
+        }
+
+        public static ILogger<T> CreateLogger<T>()
+        {
+            return _factory.CreateLogger<T>();
+        }
+    }
+}
---
+++ src/Inchoqate/GUI/Logging/FileLoggerProvider.cs
@@ -0,0 +1,22 @@
+﻿using Inchoqate.Logging;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Inchoqate.Logging
+{
+    public class FileLoggerProvider(StreamWriter logFileWriter) : ILoggerProvider
+    {
+        private readonly StreamWriter _logFileWriter =
+            logFileWriter ?? throw new ArgumentNullException(nameof(logFileWriter));
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(categoryName, _logFileWriter);
+        }
+
+        public void Dispose()
+        {
+            _logFileWriter.Dispose();
+        }
+    }
+}
