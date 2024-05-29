211112303fc2cb3239da369668e0199664e45a1c
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-29 23:28:12 +0200

    Update borderless window

---
+++ src/Inchoqate/GUI/Converters/BorderlessWinBorderThicknessConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Inchoqate.GUI.Converters
+{
+    public class BorderlessWinBorderThicknessConverter : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = (WindowState)values[0];
+            var corner = (Thickness)values[1];
+            return state == WindowState.Normal ? corner : new Thickness(0);
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Inchoqate/GUI/Converters/BorderlessWinCornerConverter.cs
@@ -0,0 +1,22 @@
+﻿using Inchoqate.GUI.Windows;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Inchoqate.GUI.Converters
+{
+    public class BorderlessWinCornerConverter : IMultiValueConverter
+    {        
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = (WindowState)values[0];
+            var corner = (CornerRadius)values[1];
+            return state == WindowState.Normal ? corner : new CornerRadius(0);
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
