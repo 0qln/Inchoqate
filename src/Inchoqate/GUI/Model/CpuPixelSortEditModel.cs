c96ae2a283c907dc37394f6c5289de5995079492
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-30 02:50:38 +0200

    Create CpuPixelSortEditModel.cs

---
+++ src/Inchoqate/GUI/Model/CpuPixelSortEditModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Inchoqate.GUI.Model
+{
+    public class CpuPixelSortEditModel : ICPUEdit
+    {
+        public bool Apply(PixelBufferModel source, PixelBufferModel destination)
+        {
+            if (source != destination)
+            {
+                // Makes the actual computation later easier.
+                // throw new ArgumentException("The source and destination parameters should be the same.");
+                return false;
+            }
+
+            // TODO
+
+            return true;
+        }
+    }
+}
