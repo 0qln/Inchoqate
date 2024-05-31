df3bcbdd1f2b4df934ccba4566e1f9a09f0c4824
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-01 01:21:11 +0200

    Update MainWindow.xaml.cs

--- src/Inchoqate/GUI/Windows/MainWindow.xaml.cs
+++ src/Inchoqate/GUI/Windows/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Controls.Primitives;
-using System.Windows.Resources;
-using System.IO;
 
 namespace Inchoqate.GUI.Windows
 {
@@ -18,10 +8,9 @@ namespace Inchoqate.GUI.Windows
     {
         private FlowchartEditorWindow? _editorWindow;
 
-
+        
         public static readonly RoutedCommand OpenFlowchartEditorCommand =
-            new RoutedCommand(
-                "OpenFlowchartEditor",
+            new("OpenFlowchartEditor",
                 typeof(MainWindow),
                 [new KeyGesture(Key.E, ModifierKeys.Control)]);
 
