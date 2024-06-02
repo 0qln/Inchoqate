79f5d595115c7339c83e6afb390d2437659c4ec8
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-03 00:00:09 +0200

    Create IEventHost.cs

---
+++ src/Inchoqate/GUI/Events/IEventHost.cs
@@ -0,0 +1,10 @@
+﻿namespace Inchoqate.GUI.Events
+{
+    public interface IEventHost
+    {
+        /// <summary>
+        /// The event manager of the event host.
+        /// </summary>
+        public EventManager EventManager { get; }
+    }
+}
