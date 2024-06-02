0e03469ce99d77a5a1fdf39e4cc27b060684c517
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-03 00:00:02 +0200

    Create Event.cs

---
+++ src/Inchoqate/GUI/Events/Event.cs
@@ -0,0 +1,33 @@
+﻿using System.Resources;
+
+namespace Inchoqate.GUI.Events
+{
+    public abstract class Event
+    {
+        /// <summary>
+        /// The previous event.
+        /// </summary>
+        public Event? Previous;
+
+        /// <summary>
+        /// The next events.
+        /// </summary>
+        public readonly List<Event> Next = [];
+
+        /// <summary>
+        /// Do the event.
+        /// </summary>
+        public abstract void Do();
+
+        /// <summary>
+        /// Undo the event.
+        /// </summary>
+        public abstract void Undo();
+    }
+
+    public sealed class InlineEvent(Action action, Action inverse) : Event
+    {
+        public override void Do() => action();
+        public override void Undo() => inverse();
+    }
+}
