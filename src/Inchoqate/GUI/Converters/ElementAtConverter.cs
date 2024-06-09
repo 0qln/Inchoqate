30da9e1ec2d5667a128ce90333fc1128f27e76fc
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-09 17:12:47 +0200

    Update ElementAtConverter.cs

--- src/Inchoqate/GUI/Converters/ElementAtConverter.cs
+++ src/Inchoqate/GUI/Converters/ElementAtConverter.cs
@@ -4,7 +4,14 @@ using System.Windows.Data;
 
 namespace Inchoqate.GUI.Converters
 {
-    public class ElementAtConverter<T>(int? index = null, T @default = default!) : IMultiValueConverter
+    // The 'fallbackPredicate' dertermines whether the fallback value should be returned or not.
+    // The 'IndexTransform' is used to transform the index before it is used.
+    public class ElementAtConverter<T>(
+        int? index = null,
+        Func<int, int>? indexTransform = null,
+        T @default = default!,
+        Predicate<int>? fallbackPredicateIndex = null,
+        Predicate<T>? fallbackPredicateValue = null) : IMultiValueConverter
         where T : notnull
     {
         private readonly int? _initIndex = index;
@@ -23,13 +30,27 @@ namespace Inchoqate.GUI.Converters
                 throw new ArgumentException("No index specified");
             }
 
-            return values[0] switch
+            index = indexTransform?.Invoke(index.Value) ?? index.Value;
+
+            if (fallbackPredicateIndex?.Invoke(index.Value) ?? false)
+            {
+                return @default;
+            }
+
+            T result = values[0] switch
             {
                 null => @default,
                 T[] arr => arr[index.Value],
                 ObservableCollection<T> col => col[index.Value],
                 _ => throw new NotSupportedException(),
             };
+
+            if (fallbackPredicateValue?.Invoke(result) ?? false)
+            {
+                return @default;
+            }
+
+            return result; 
         }
 
         public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
