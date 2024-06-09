4af53fd6b40d5277bc73f12c961a49b3edb09006
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-09 17:36:50 +0200

    Create Vector3ToDoubleArrConverter.cs

---
+++ src/Inchoqate/GUI/Converters/Vector3ToDoubleArrConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Data;
+using OpenTK.Mathematics;
+using System.Globalization;
+
+namespace Inchoqate.GUI.Converters
+{
+    public class Vector3ToDoubleArrConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vec = (Vector3)value;
+            return new double[] { vec.X, vec.Y, vec.Z };
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var arr = (double[])value;
+            return new Vector3((float)arr[0], (float)arr[1], (float)arr[2]);
+        }
+    }
+}
