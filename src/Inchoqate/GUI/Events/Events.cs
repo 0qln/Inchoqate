e4870faf58ace6c73561d0f41de11a44f7bef8a9
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-11 14:46:04 +0200

    Update Events.cs

--- src/Inchoqate/GUI/Events/Events.cs
+++ src/Inchoqate/GUI/Events/Events.cs
@@ -15,8 +15,5 @@ public class ItemAdded<T>(ObservableCollectionBase<T> param, T item)
     : Event<ObservableCollectionBase<T>>(param)
 {
     public override void Apply(ObservableCollectionBase<T> @object) => @object.Add(item);
-    public void Apply<T2>(ObservableCollectionBase<T2> @object, Func<T, T2> converter) => @object.Add(converter(item));
-
     public override void Revert(ObservableCollectionBase<T> @object) => @object.Remove(item);
-    public void Revert<T2>(ObservableCollectionBase<T2> @object, Func<T, T2> converter) => @object.Remove(converter(item));
 }
