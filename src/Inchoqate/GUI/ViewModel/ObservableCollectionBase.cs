2010a2d6628404e0a474bcde7deea326dbbe26f2
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-11 14:33:02 +0200

    Update ObservableCollectionBase.cs

--- src/Inchoqate/GUI/ViewModel/ObservableCollectionBase.cs
+++ src/Inchoqate/GUI/ViewModel/ObservableCollectionBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace Inchoqate.GUI.ViewModel
 {
@@ -14,6 +15,56 @@ namespace Inchoqate.GUI.ViewModel
 
         public ObservableCollectionBase(List<T> list) : base(list)
         {
+        }
+
+        public static ObservableCollectionBase<T> Mirror<TOther>(ObservableCollectionBase<TOther> other, Func<TOther, T> cast)
+        {
+            // initiate
+            var result = new ObservableCollectionBase<T>();
+            foreach (var item in other)
+            {
+                result.Add(cast(item));
+            }
+
+            // set up mirror
+            other.CollectionChanged += (s, e) =>
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        if (e.NewItems is null)
+                            throw new ArgumentException("e.NewItems is null");
+                        foreach (var item in e.NewItems)
+                            result.Insert(e.NewStartingIndex, cast((TOther)item));
+                        break;
+                    case NotifyCollectionChangedAction.Remove:
+                        if (e.OldItems is null)
+                            throw new ArgumentException("e.OldItems is null");
+                        foreach (var item in e.OldItems)
+                            result.Remove(cast((TOther)item));
+                        break;
+                    case NotifyCollectionChangedAction.Replace:
+                        if (e.OldItems is null)
+                            throw new ArgumentException("e.OldItems is null");
+                        if (e.NewItems is null)
+                            throw new ArgumentException("e.NewItems is null");
+                        foreach (var item in e.OldItems)
+                            result.Remove(cast((TOther)item));
+                        foreach (var item in e.NewItems)
+                            result.Insert(e.NewStartingIndex, cast((TOther)item));
+                        break;
+                    case NotifyCollectionChangedAction.Move:
+                        result.Move(e.OldStartingIndex, e.NewStartingIndex);
+                        break;
+                    case NotifyCollectionChangedAction.Reset:
+                        result.Clear();
+                        foreach (var item in other)
+                            result.Add(cast(item));
+                        break;
+                }
+            };
+
+            return result;
         }
     }
 }
