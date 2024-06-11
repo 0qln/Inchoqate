74b2bd8674b490c1f41f43c81c1c8467dce9fc01
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-11 18:17:18 +0200

    Fix resource leak

--- src/Inchoqate/GUI/View/ExtSliderView.xaml.cs
+++ src/Inchoqate/GUI/View/ExtSliderView.xaml.cs
@@ -409,6 +409,24 @@ namespace Inchoqate.GUI.View
                     -1,
                     FrameworkPropertyMetadataOptions.AffectsRender));
 
+        public static readonly DependencyProperty TextBrushProperty =
+            DependencyProperty.Register(
+                "TextBrush",
+                typeof(Brush),
+                typeof(SliderInfoAdorner),
+                new FrameworkPropertyMetadata(
+                    new SolidColorBrush((Color)((App)Application.Current).ThemeDictionary["Text_2"]),
+                    FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty BackgroundBrushProperty =
+            DependencyProperty.Register(
+                "BackgroundBrush",
+                typeof(Brush),
+                typeof(SliderInfoAdorner),
+                new FrameworkPropertyMetadata(
+                    new SolidColorBrush((Color)((App)Application.Current).ThemeDictionary["Popup_Idle_1"]),
+                    FrameworkPropertyMetadataOptions.AffectsRender));
+
 
         public double Maximum
         {
@@ -458,14 +476,16 @@ namespace Inchoqate.GUI.View
             set { SetValue(IndexProperty, value); }
         }
 
-        public Brush TextColor
+        public Brush TextBrush
         {
-            get => new SolidColorBrush((Color)((App)Application.Current).ThemeDictionary["Text_2"]);
+            get { return (Brush)GetValue(TextBrushProperty); }
+            set { SetValue(TextBrushProperty, value); }
         }
 
-        public Brush BackgroundColor
+        public Brush BackgroundBrush
         {
-            get => new SolidColorBrush((Color)((App)Application.Current).ThemeDictionary["Popup_Idle_1"]);
+            get { return (Brush)GetValue(BackgroundBrushProperty); }
+            set { SetValue(BackgroundBrushProperty, value); }
         }
 
 
@@ -479,6 +499,7 @@ namespace Inchoqate.GUI.View
             SetBinding(ShowPrevRangeProperty, new Binding("ShowPrevRange") { Source = adornedElement, });
             SetBinding(ShowNextRangeProperty, new Binding("ShowNextRange") { Source = adornedElement, });
         }
+
 
         protected override void OnRender(DrawingContext drawingContext)
         {
@@ -505,7 +526,7 @@ namespace Inchoqate.GUI.View
                         FlowDirection.LeftToRight,
                         typeFace,
                         textSize,
-                        TextColor)
+                        TextBrush)
                     {
                         MaxTextWidth = maxTextWidth,
                     };
@@ -516,7 +537,7 @@ namespace Inchoqate.GUI.View
                         ? -formattedText.Height + (slider.ActualHeight - track.Thumb.ActualHeight)
                         : (slider.ActualHeight - track.Thumb.ActualHeight) + track.Thumb.ActualHeight;
                     // draw
-                    if (top) drawingContext.DrawRectangle(BackgroundColor, null, new Rect(x, y, formattedText.Width, formattedText.Height));
+                    if (top) drawingContext.DrawRectangle(BackgroundBrush, null, new Rect(x, y, formattedText.Width, formattedText.Height));
                     drawingContext.DrawText(formattedText, new Point(x, y));
                 }
 
@@ -616,7 +637,31 @@ namespace Inchoqate.GUI.View
                 typeof(SliderPart),
                 new FrameworkPropertyMetadata(
                     null,
-                    FrameworkPropertyMetadataOptions.AffectsRender));
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    propertyChangedCallback: ExtSliderChangedCallback));
+
+        private static void ExtSliderChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var @this = (SliderPart)d;
+            var bgBinding = new MultiBinding() { Converter = new GradientStopsConverter() };
+            bgBinding.Bindings.Add(new Binding("BackgroundGradientBrushes") { Source = e.NewValue });
+            bgBinding.Bindings.Add(new Binding("Values") { Source = e.NewValue });
+            bgBinding.Bindings.Add(new Binding("Minimum") { Source = e.NewValue });
+            bgBinding.Bindings.Add(new Binding("Maximum") { Source = e.NewValue });
+            @this.SetBinding(BackgroundProperty, bgBinding);
+
+            @this._infoAdorner = new SliderInfoAdorner(@this);
+            AdornerLayer.GetAdornerLayer(@this).Add(@this._infoAdorner);
+            var prBinding = new MultiBinding() { Converter = new ElementAtConverter<bool>(fallbackPredicateIndex: index => index != 0) };
+            prBinding.Bindings.Add(new Binding("ShowRanges") { Source = e.NewValue });
+            prBinding.Bindings.Add(new Binding("Index") { Source = @this });
+            @this._infoAdorner.SetBinding(SliderInfoAdorner.ShowPrevRangeProperty, prBinding);
+            var nrBinding = new MultiBinding() { Converter = new ElementAtConverter<bool>(indexTransform: index => index + 1) };
+            nrBinding.Bindings.Add(new Binding("ShowRanges") { Source = e.NewValue });
+            nrBinding.Bindings.Add(new Binding("Index") { Source = @this });
+            @this._infoAdorner.SetBinding(SliderInfoAdorner.ShowNextRangeProperty, nrBinding);
+            @this._infoAdorner.SetBinding(SliderInfoAdorner.RangesProperty, new Binding("Ranges") { Source = e.NewValue });
+        }
 
         public static readonly DependencyProperty TrackVisibilityProperty =
             DependencyProperty.Register(
@@ -711,6 +756,9 @@ namespace Inchoqate.GUI.View
         }
 
 
+        private Adorner? _infoAdorner;
+
+
         static SliderPart()
         {
             ValueProperty.OverrideMetadata(
@@ -728,29 +776,6 @@ namespace Inchoqate.GUI.View
         {
             SetBinding(RangeProperty, new Binding("Value") { Source = this, Converter = new ValueToRangeConverter(previousPart), Mode = BindingMode.TwoWay });
             SetBinding(TrackVisibilityProperty, new Binding("Index") { Source = this, Converter = new IndexToTrackVisibilityConverter(), Mode = BindingMode.OneWay });
-            Loaded += LoadedHandler;
-        }
-
-        private void LoadedHandler(object sender, RoutedEventArgs e)
-        {
-            var adorner = new SliderInfoAdorner(this);
-            AdornerLayer.GetAdornerLayer(this).Add(adorner);
-            var prBinding = new MultiBinding() { Converter = new ElementAtConverter<bool>(fallbackPredicateIndex: index => index != 0) };
-            prBinding.Bindings.Add(new Binding("ShowRanges") { Source = ExtSlider });
-            prBinding.Bindings.Add(new Binding("Index") { Source = this });
-            adorner.SetBinding(SliderInfoAdorner.ShowPrevRangeProperty, prBinding);
-            var nrBinding = new MultiBinding() { Converter = new ElementAtConverter<bool>(indexTransform: index => index + 1) };
-            nrBinding.Bindings.Add(new Binding("ShowRanges") { Source = ExtSlider });
-            nrBinding.Bindings.Add(new Binding("Index") { Source = this });
-            adorner.SetBinding(SliderInfoAdorner.ShowNextRangeProperty, nrBinding);
-            adorner.SetBinding(SliderInfoAdorner.RangesProperty, new Binding("Ranges") { Source = ExtSlider });
-
-            var bgBinding = new MultiBinding() { Converter = new GradientStopsConverter() };
-            bgBinding.Bindings.Add(new Binding("BackgroundGradientBrushes") { Source = ExtSlider });
-            bgBinding.Bindings.Add(new Binding("Values") { Source = ExtSlider });
-            bgBinding.Bindings.Add(new Binding("Minimum") { Source = ExtSlider });
-            bgBinding.Bindings.Add(new Binding("Maximum") { Source = ExtSlider });
-            SetBinding(BackgroundProperty, bgBinding);
         }
 
         private static void ValuepropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
