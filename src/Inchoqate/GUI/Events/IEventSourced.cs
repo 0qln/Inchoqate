ed4dea445510767a9141d521bd7a1736cedd8d2c
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-11 14:34:53 +0200

    Create IEventSourced.cs

---
+++ src/Inchoqate/GUI/Events/IEventSourced.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchoqate.GUI.Events
+{
+    public interface IEventSourced
+    {
+        public Guid Id { get; init; }
+
+        public void Apply(Event @event);
+    }
+}
