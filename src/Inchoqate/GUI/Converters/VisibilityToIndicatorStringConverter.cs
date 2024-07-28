9d7a3a183befcaecf63691a0c4c2c391a29a30f9
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-07-28 23:18:28 +0200

    Bug fix

--- src/Inchoqate/GUI/Converters/VisibilityToIndicatorStringConverter.cs
+++ src/Inchoqate/GUI/Converters/VisibilityToIndicatorStringConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
@@ -9,24 +10,26 @@ namespace Inchoqate.GUI.Converters
     {
         public const string Visible     = "⌵";
         public const string Collapsed   = ">";
+        public const string Hidden      = Collapsed;
 
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        object IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return (Visibility)value switch
+            return (Visibility?)value switch
             {
                 Visibility.Visible      => Visible,
                 Visibility.Collapsed    => Collapsed,
-                _ => throw new NotImplementedException()
+                Visibility.Hidden       => Hidden,
+                _ => throw new UnreachableException()
             };
         }
 
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        object IValueConverter.ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return (string)value switch
+            return (string?)value switch
             {
-                Visible => Visibility.Visible,
-                Collapsed => Visibility.Collapsed,
-                _ => throw new NotImplementedException()
+                Visible     => Visibility.Visible,
+                Collapsed   => Visibility.Collapsed,
+                _ => throw new UnreachableException()
             };
         }
     }
